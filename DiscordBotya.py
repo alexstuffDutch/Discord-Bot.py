d28679c99ad80d7613416479f583aca1c92bc4b7
alexstuffDutch <40435366+alexstuffDutch@users.noreply.github.com>
2018-09-17 19:38:32 +0200

    Update DiscordBotya.py

--- DiscordBotya.py
+++ DiscordBotya.py
@@ -9,69 +9,18 @@ client = commands.Bot(command_prefix="?")
 
 @client.event
 async def on_message(message):
-    if message.content.upper().startswith('P!REPORT'):
+    if message.content.upper().startswith('!SALES'):
         userID = message.author.id
-        server = client.get_server("363386694868664320")
+        server = client.get_server("490938256683302912")
         args = message.content.split(" ")
-        await client.send_message(server.get_channel("444592474699071508"),  "%s" % (" ".join(args[1:])))
-        await client.send_message(message.author, "Send **succ**essfully!")
-        await client.send_message(server.get_channel("444924917763407892"), "<@%s> reported" % (userID))
-                
-    if message.content.upper().startswith('P!INVITE'):
-        await client.send_message(message.author, "https://discord.gg/CQp5rdJ")
-        userID = message.author.id
-        server = client.get_server("363386694868664320")
-        await client.send_message(server.get_channel("444924917763407892"), "<@%s> requested a invite link." % (userID))
-        await client.send_message(message.channel, "<@%s> Check your DMs!" % (userID))
-
-    if message.content.upper().startswith('P!SHUTDOWN'):
-        if message.author.id == "346546938490912768":
-            server = client.get_server("363386694868664320")
-            await client.send_message(message.channel, "Announcing shutdown!")
-            await client.send_message(server.get_channel("444857761587527681"), "Bot is going to shutdown!")
-            await client.change_presence(game=discord.Game(name='Going to shutdown!'))
-        else:
-            await client.send_message(message.channel, "You do not have permission!")
-
-    if message.content.upper().startswith('P!HELP'):
-        embed=discord.Embed(title="Bot help", description="Help list", color=0x00ff00)
-        embed.add_field(name="Commands", value="Just say p!cmds for commands", inline=False)
-        embed.add_field(name="About", value="This bot is a bot for Pizza People, the goal of this bot is to make Pizza People safer, cleaner and more efficient. Its goal is also to bring up fun and happyness.", inline=False)
-        await client.send_message(message.channel, embed=embed)
-        
-    if message.content.upper().startswith('P!QOTD'):
-        if "444592342293282820" in [role.id for role in message.author.roles]:
-            userID = message.author.id
-            server = client.get_server("363386694868664320")
-            args = message.content.split(" ")
-            await client.send_message(server.get_channel("459021735807811594"),  "%s" % (" ".join(args[1:])))   
-            await client.send_message(server.get_channel("444924917763407892"), "<@%s> set the QOTD" % (userID))
-        else:
-            await client.send_message(message.channel, "You are not allowed")
-
-
-    if message.content.upper().startswith('P!CMDS'):
-        embed=discord.Embed(title="Commands", description="Commands list", color=0x00ff00)
-        embed.add_field(name="P!REPORT", value="This will let admins know with the message you have given, but you are most likely to be questioned, and also, abusing this will get you to a warning. Use this like this: P!REPORT [MESSAGE].", inline=False)
-        embed.add_field(name="P!INVITE", value="This will let you show an invite for inviting people.", inline=False)
-        embed.add_field(name="Admin Commands", value="Under is for admins, regular people cannot access these commands.", inline=False)
-        embed.add_field(name="P!QOTD", value="Lets the bot say the QOTD in the #qotd channel. Use it like this: P!QOTD [QOTD].", inline=False)
-        embed.add_field(name="----", value="Currently in progress, please do not steal the script, thank you.")
-        await client.send_message(message.channel, embed=embed)
-            
-            
-        
-                      
-        
-        
-
-                                             
+        await client.send_message(server.get_channel("490942629916311562"), "%s" % (" ".join(args[1:])))
+        await client.send_message(message.author, "A salesman will contact you! Thank you for buying!")
+        await client.send_message(server.get_channel("490942613269250048"), "<@%s> requested a sale!" % (userID))
+                   
 @client.event
 async def on_ready():
-    server = client.get_server("363386694868664320")
-    await client.send_message(server.get_channel("444857761587527681"), "Bot is ready and up! Current version is 0.2.8")
-    await client.change_presence(game=discord.Game(name="Making pizzas!"))
+    await client.change_presence(game=discord.Game(name="Handling sales!"))
     
 
         
-client.run("NDQ0MTYxNjQ3MDI4ODYyOTc2.DdjJgg.lsgqAMpYLLIqJunIR4t70Xx-fa8")
+client.run("NDkwOTQyNTAxOTQ4MjI3NTk3.DoApmg.B3NfNYJ32bCw93ZTdEsKRpvV0eM")
